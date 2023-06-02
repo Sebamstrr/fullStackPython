5a81295768d16810531fee0abad246fc0f106977
Sebamstrr <mccrash2018@gmail.com>
2023-06-02 11:20:10 -0400

    ok

---
+++ metodo.py
@@ -0,0 +1,19 @@
+class Usuario:
+    def __init__(self, nombre):
+        self.nombre = nombre
+        self.saldo = 0
+    
+    def hacer_deposito(self, monto):
+        self.saldo += monto
+        return self
+    
+    def hacer_retiro(self, monto):
+        self.saldo -= monto
+        return self
+    
+    def mostrar_balance_usuario(self):
+        print(f"El saldo de {self.nombre} es: {self.saldo}")
+        return self
+
+guido = Usuario("Guido")
+guido.hacer_deposito(100).hacer_deposito(200).hacer_deposito(300).hacer_retiro(50).mostrar_balance_usuario()
