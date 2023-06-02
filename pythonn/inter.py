108f03db3d147c44ab6f8677196225d59dd2bb92
Sebamstrr <mccrash2018@gmail.com>
2023-06-02 11:16:49 -0400

    ok

--- pythonn/inter.py
+++ pythonn/inter.py
@@ -24,3 +24,47 @@ print(directorio_deportes)  # {'basketball': ['Kobe', 'Jordan', 'James', 'Curry'
 # Cambiar el valor 20 en z a 30
 z[0]['y'] = 30
 print(z)  # [{'x': 10, 'y': 30}]
+
+estudiantes = [
+    {'first_name': 'Michael', 'last_name': 'Jordan'},
+    {'first_name': 'John', 'last_name': 'Rosales'},
+    {'first_name': 'Mark', 'last_name': 'Guillen'},
+    {'first_name': 'KB', 'last_name': 'Tonel'}
+]
+
+def iterateDictionary(some_list):
+    for item in some_list:
+        for key, value in item.items():
+            print(f"{key} - {value}")
+
+iterateDictionary(estudiantes)
+# Salida:
+# first_name - Michael, last_name - Jordan
+# first_name - John, last_name - Rosales
+# first_name - Mark, last_name - Guillen
+# first_name - KB, last_name - Tonel
+
+def iterateDictionary2(key_name, some_list):
+    for item in some_list:
+        print(item[key_name])
+
+iterateDictionary2('first_name', estudiantes)
+# Salida:
+# Michael
+# John
+# Mark
+# KB
+
+iterateDictionary2('last_name', estudiantes)
+# Salida:
+# Jordan
+# Rosales
+# Guillen
+# Tonel
+
+def printInfo(some_dict):
+    for key, value in some_dict.items():
+        print(f"{len(value)} {key.upper()}")
+        for item in value:
+            print(item)
+
